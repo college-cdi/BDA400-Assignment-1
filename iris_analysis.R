7aa3ba460878be0a6adb3e2f02a240389fe54a8c
college-cdi <sukhdevsingh1@hotmail.com>
2026-09-16 21:30:59 -0400

    Add files via upload

---
+++ iris_analysis.R
@@ -0,0 +1,28 @@
+# Iris Flower Data Analysis
+# Load the built-in iris dataset
+data(iris)
+#View the first six rows
+head(iris)
+# Display dataset information
+str(iris)
+# calculate summary statistics
+summary(iris)
+# calculate average sepal lenght by species
+aggregate(Sepal.Length~Species, data=iris, Fun=mean)
+# Create a scatter plot
+plot(
+  iris$Sepal.Length,
+  iris$Petal.Length,
+  main = "Sepal Length vs Petal Length",
+  xlab = "Sepal Length",
+  ylab = "Petal Length",
+  col = iris$Species,
+  pch = 19
+)
+# Add a legend
+legend(
+  "topleft",
+  legend = level(iris$species),
+  col =1:3,
+  pch = 19
+)
